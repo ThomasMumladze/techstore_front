d8261c6a3f482789abfa3d3c25c51d7cb8ff2d67
ThomasMumladze <mumladzea4@gmail.com>
2026-04-16 14:30:39 +0400

    app configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navigation from "./components/Navigation";
+
+function App() {
+    return (
+        <>
+            <Navigation />
+            لإنسان
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,21 @@
+import "../styles/components/_navigation.scss";
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            <ul>
+                <li>
+                    <a href="">shop</a>
+                </li>
+                <li>
+                    <a href="">about</a>
+                </li>
+                <li>
+                    <a href="">contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
