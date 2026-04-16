b737ea37de9730a86b82ef8bec5e8bcd8500ce53
ThomasMumladze <mumladzea4@gmail.com>
2026-04-16 18:13:32 +0400

    dev

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./styles/components/_navigation.scss";
 import "./styles/components/_dropDawnMenu.scss";
 import "./styles/layouts/_header.scss";
 import "./styles/pages/_home.scss";
+import "./styles/pages/_contact.scss";
 
 import { RouterProvider } from "react-router";
 
